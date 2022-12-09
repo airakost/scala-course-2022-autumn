a39c996be9e4fe44655f7bb7ae7ab24c70e4d540
airamariya <kostenko2020.8954117@student.karazin.ua>
2022-12-09 22:42:42 +0100

    Week-3 - Add solution for peano numbers

--- src/main/scala/karazin/scala/users/group/week3/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week3/Homework.scala
@@ -1,50 +1,62 @@
 package karazin.scala.users.group.week3
 
 object Homework:
-  
+
   // Peano numbers
   abstract class Nat:
     def isZero: Boolean
     def predecessor: Nat
-    def successor: Nat = ???
-    
+    def successor: Nat = new Succ(this)
+
     infix def + (that: Nat): Nat
-    
+
     infix def - (that: Nat): Nat
-    
+
     // Optional task
-    def toInt: Int
-    
+    // def toInt: Int
+
     // Optional task
-    def fromInt(int: Int) = ???
-  
+    // def fromInt(int: Int) = ???
+
     override def toString: String = s"Nat($predecessor)"
-  
-  type Zero = Zero.type 
+
+  type Zero = Zero.type
   object Zero extends Nat:
     def isZero: Boolean = true
     def predecessor: Nat = throw new Exception("0 doesn't have a predecessor")
-    
-    infix def +(that: Nat): Nat = ???
-    
-    infix def -(that: Nat): Nat = ???
-    
+
+    infix def +(that: Nat): Nat = that
+
+    infix def -(that: Nat): Nat =
+      // parameters validation
+      require(that.isZero)
+      this
+
     // Optional task
-    def toInt: Int = ???
+    // def toInt: Int = ???
 
     override def toString: String = "Zero"
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match {
+      case zero: Zero => true
+      case _ => false
+    }
 
   class Succ(n: Nat) extends Nat:
     def isZero: Boolean = false
-    def predecessor: Nat = ???
-    
-    infix def +(that: Nat): Nat = ???
-    
-    infix def -(that: Nat): Nat = ???
-    
+    def predecessor: Nat = n
+
+    infix def +(that: Nat): Nat = new Succ(that + n)
+
+    infix def -(that: Nat): Nat = that.isZero match {
+      case true => this
+      case _ => n - that.predecessor
+    }
+
     // Optional task
-    def toInt: Int = ???
+    // def toInt: Int = ???
 
-    override def equals(obj: Any): Boolean = ???
-
+    override def equals(obj: Any): Boolean = obj match {
+      case n: Nat => this.predecessor == n.predecessor
+      case zero: Zero => false
+      case _ => false
+    }
